2d8e5ff716f62eb4dadce9176888232fb75d7ac2
Bohdan <bohdan.korolev@gmail.com>
2019-10-23 07:35:21 +0300

    All unit-tests completed

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime endDate = startDate;
+            int weekendsDuration = 1;
+
+            if (weekEnds == null)
+            {
+                endDate = startDate.AddDays(dayCount - 1);
+            }
+            else
+            {
+                for (int i = 1, j = 0; i < dayCount; i++)
+                {
+                    if ((j < weekEnds.Length) && (endDate == weekEnds[j].StartDate))
+                    {
+                        weekendsDuration += weekEnds[j].EndDate.Day - weekEnds[j].StartDate.Day;
+                        endDate = endDate.AddDays(weekendsDuration);
+                        j++;
+                    }
+                    endDate = endDate.AddDays(1);
+                }
+            }
+            return endDate;
+        }
+    }
+}
